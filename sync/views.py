6552e9c53adbc72a10522cb09960dab04deb73a8
Manish Yadav <my68tron@gmail.com>
2020-04-08 23:13:17 +0530

    Updated sync app

--- sync/views.py
+++ sync/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+from sync.models import Contact
 import json
+import csv
 import os
 
 # Index page of sync with details and rules to setup sync
@@ -18,11 +20,28 @@ def initialise(request):
             return HttpResponse('OK')
     return HttpResponseBadRequest("Bad Request")
 
-# Update Android data
+
+def handle_uploaded_file(f):
+    name = os.path.join(settings.MEDIA_DIR, f.name + ".csv")
+    with open(name, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    return name
+
+# Update Contact data
 @csrf_exempt
 def update(request):
     if request.method == 'POST':
-        request_body = json.loads(request.POST['jsonData'])
-        if request.FILES.getlist('uploadedFile') != None:
-            return HttpResponse('OK')
+        filesList = request.FILES.getlist('contacts_info')
+        fileName = handle_uploaded_file(filesList[0])
+        from_mob_no = int(filesList[0].name)
+        with open(fileName, 'r') as f:
+            index = 0
+            csvFile = csv.reader(f)
+            for row in csvFile:
+                contact = Contact.objects.create(from_mob_no=from_mob_no, to_mob_no=int(row[0]), timestamp=row[1])
+                index += 1
+        os.remove(fileName)
+        print('Total Contacts :', index)
+        return JsonResponse({'total_contacts': index})
     return HttpResponseBadRequest("Bad Request")
