b3b4c79cd32d9e14b21ffa3a506631ec84b5f46b
Manish Yadav <my68tron@gmail.com>
2020-04-10 17:33:14 +0530

    Updated settings

--- ContactTrackerWebApp/settings.py
+++ ContactTrackerWebApp/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 from decouple import config
+import whitenoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
