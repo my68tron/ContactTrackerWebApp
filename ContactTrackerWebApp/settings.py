1881f2f211bb52d3c5ec6770134a584fb5993e5e
Manish Yadav <my68tron@gmail.com>
2020-04-10 15:33:38 +0530

    Changes Settings

--- ContactTrackerWebApp/settings.py
+++ ContactTrackerWebApp/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'ContactTrackerWebApp.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600, ssl_require=False)
 
 # Password validation
