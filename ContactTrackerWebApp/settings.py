cdf86951b27bec598765191b1dd0d545f08996cc
Manish Yadav <my68tron@gmail.com>
2020-04-10 15:08:41 +0530

    Added SB Setting

--- ContactTrackerWebApp/settings.py
+++ ContactTrackerWebApp/settings.py
@@ -90,9 +90,12 @@ WSGI_APPLICATION = 'ContactTrackerWebApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'url': os.environ('DATABASE_URL')
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
